aa07f7dbfb203ba18888e42a04c523079304b2d2
Ronny Li <ronnyli91@gmail.com>
2020-04-14 16:59:06 -0400

    files to set up GCP instance

---
+++ gcloud_commands.sh
@@ -0,0 +1,51 @@
+git clone
+cd headless-ib-gateway-installation-ubuntu-server
+# EDIT IBController.ini with your username/password
+
+gcloud create project leverheads
+
+gcloud beta compute \
+--project=leverheads instances create ib-gateway \
+--zone=northamerica-northeast1-a \
+--machine-type=g1-small \
+--subnet=default \
+--network-tier=PREMIUM \
+--metadata-from-file=startup-script=scratch.sh \
+--maintenance-policy=MIGRATE \
+--service-account=599937284915-compute@developer.gserviceaccount.com \
+--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+--tags=http-server,https-server \
+--image=ubuntu-1804-bionic-v20200317 \
+--image-project=ubuntu-os-cloud \
+--boot-disk-size=10GB \
+--boot-disk-type=pd-standard \
+--boot-disk-device-name=ib-gateway \
+--no-shielded-secure-boot \
+--shielded-vtpm \
+--shielded-integrity-monitoring \
+--reservation-affinity=any
+
+echo | gcloud compute scp --zone northamerica-northeast1-a jts.ini test3:~
+echo | gcloud compute scp --zone northamerica-northeast1-a IBControllerGatewayStart.sh test3:~
+echo | gcloud compute scp --zone northamerica-northeast1-a IBController.ini test3:~
+
+gcloud compute \
+--project=leverheads firewall-rules create default-allow-http \
+--direction=INGRESS \
+--priority=1000 \
+--network=default \
+--action=ALLOW \
+--rules=tcp:80 \
+--source-ranges=0.0.0.0/0 \
+--target-tags=http-server
+
+
+gcloud compute \
+--project=leverheads firewall-rules create default-allow-https \
+--direction=INGRESS \
+--priority=1000 \
+--network=default \
+--action=ALLOW \
+--rules=tcp:443 \
+--source-ranges=0.0.0.0/0 \
+--target-tags=https-server
--- gcp-setup.sh
+++ gcp-setup.sh
@@ -4,37 +4,58 @@
 # This is a work-in-progress file for properly configuring a server
 # DO NOT RUN YET
 
-# TODO: copy everything from the headless ib gateway README
+# Everything from the headless ib gateway README
+sudo -i
+cd ~
+apt update
+apt install -y unzip xvfb x11vnc
+echo | Xvfb :1 -ac -screen 0 1024x768x24 &
+
+# Make sure DISPLAY=:1 is a permanent environment variable
 echo DISPLAY=:1 >> ~/.bashrc
 echo DISPLAY=:1 >> ~/.profile 
-echo DISPLAY=:1 >> /etc/environment  # This one needs to be done as root
+echo DISPLAY=:1 >> /etc/environment
 source ~/.bashrc
 source ~/.profile
 
-# Install Python
-sudo apt update
-sudo apt install -y python3 python3-dev python3-venv gcc  # gcc is for bt dependency
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
+# in your root's home directory
+cd ~
+# download installation script
+wget https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/ibgateway-latest-standalone-linux-x64.sh
+# make it executable
+chmod a+x ibgateway-latest-standalone-linux-x64.sh
+# run it
+echo | sh ibgateway-latest-standalone-linux-x64.sh -c
 
-# virtualenv
-mkdir venv
-python3 -m venv venv/leverhead
-source venv/leverhead/bin/activate
-pip install -U pip
+# Copy configuration files
 
-# Clone leverhead
-# git clone
-cd leverhead
-pip install -r requirements.txt
 
-# Create keys needed for IB authentication
-openssl dhparam -outform PEM 2048 -out dhparam.pem
-openssl genrsa -out private_signature.pem 2048
-openssl genrsa -out private_encryption.pem 2048
-openssl rsa -in private_signature.pem -outform PEM -pubout -out public_signature.pem
-openssl rsa -in private_encryption.pem -outform PEM -pubout -out public_encryption.pem
+# Start IB Gateway and send to TightVNC
+# DISPLAY=:1 ~/ibcontroller.paper/IBControllerGatewayStart.sh
 
-# Run cron
-# TODO: need a cron that restarts IB Gateway every week
-# 50 19 * * 1-5 /usr/bin/env bash -c 'source /home/support/venv/leverheads/bin/activate && python /home/support/leverhead/LETF_strategy_vol_hedging.py' >> /home/support/leverhead/log/cron_output 2>&1
+# # Install Python
+# apt install -y python3 python3-dev python3-venv gcc  # gcc is for bt dependency
+# wget https://bootstrap.pypa.io/get-pip.py
+# sudo python3 get-pip.py
+
+# # virtualenv
+# mkdir venv
+# python3 -m venv venv/leverhead
+# source venv/leverhead/bin/activate
+# pip install -U pip
+
+# # Clone leverhead
+# # git clone
+# cd leverhead
+# pip install -r requirements.txt
+
+# # Create keys needed for IB authentication
+# openssl dhparam -outform PEM 2048 -out dhparam.pem
+# openssl genrsa -out private_signature.pem 2048
+# openssl genrsa -out private_encryption.pem 2048
+# openssl rsa -in private_signature.pem -outform PEM -pubout -out public_signature.pem
+# openssl rsa -in private_encryption.pem -outform PEM -pubout -out public_encryption.pem
+
+# # Run cron
+# # TODO: need a cron that restarts IB Gateway every week
+# # 50 19 * * 1-5 /usr/bin/env bash -c 'source /home/support/venv/leverheads/bin/activate && python /home/support/leverhead/LETF_strategy_vol_hedging.py' >> /home/support/leverhead/log/cron_output 2>&1
