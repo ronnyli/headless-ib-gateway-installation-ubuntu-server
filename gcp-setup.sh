58c0ccd40a5f3722aa8649fcd40f35fba4f27af3
Ronny Li <ronnyli91@gmail.com>
2020-04-15 14:57:26 -0400

    remove comments at end of file

--- gcp-setup.sh
+++ gcp-setup.sh
@@ -1,10 +1,11 @@
 ####################
-## WARNING! WARNING!
+## This script is meant to be run on a GCP instance
 ####################
-# This is a work-in-progress file for properly configuring a server
-# DO NOT RUN YET
 
-# Everything from the headless ib gateway README
+#####
+# Step 1: Set up IB Gateway server
+#####
+# Install dependencies
 PWD_OUTPUT_USER=$(pwd)
 cd /root/
 apt update && apt install -y unzip xvfb x11vnc
@@ -58,32 +59,3 @@ DISPLAY=:1 /root/ibcontroller.paper/IBControllerGatewayStart.sh
 
 
 echo 'Run IBControllerGateway' >> /tmp/gcp-setup.log
-
-
-exit
-# # Install Python
-# apt install -y python3 python3-dev python3-venv gcc  # gcc is for bt dependency
-# wget https://bootstrap.pypa.io/get-pip.py
-# sudo python3 get-pip.py
-
-# # virtualenv
-# mkdir venv
-# python3 -m venv venv/leverhead
-# source venv/leverhead/bin/activate
-# pip install -U pip
-
-# # Clone leverhead
-# # git clone
-# cd leverhead
-# pip install -r requirements.txt
-
-# # Create keys needed for IB authentication
-# openssl dhparam -outform PEM 2048 -out dhparam.pem
-# openssl genrsa -out private_signature.pem 2048
-# openssl genrsa -out private_encryption.pem 2048
-# openssl rsa -in private_signature.pem -outform PEM -pubout -out public_signature.pem
-# openssl rsa -in private_encryption.pem -outform PEM -pubout -out public_encryption.pem
-
-# # Run cron
-# # TODO: need a cron that restarts IB Gateway every week
-# # 50 19 * * 1-5 /usr/bin/env bash -c 'source /home/support/venv/leverheads/bin/activate && python /home/support/leverhead/LETF_strategy_vol_hedging.py' >> /home/support/leverhead/log/cron_output 2>&1
