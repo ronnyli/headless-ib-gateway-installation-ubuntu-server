8cbcc4a89df92bddcb6d6bc990b404f13cf752f6
Ronny Li <ronnyli91@gmail.com>
2020-08-28 14:43:32 -0400

    warm restart hints

--- gcp-setup.sh
+++ gcp-setup.sh
@@ -6,11 +6,11 @@
 # Step 1: Set up IB Gateway server
 #####
 # Install dependencies
-cd config
-CONFIG_DIR=$(pwd)
-cd /root/
+cd config  # WARM RESTART
+CONFIG_DIR=$(pwd)  # WARM RESTART
+cd /root/  # WARM RESTART
 apt-get update && apt-get install -y unzip xvfb x11vnc
-echo | Xvfb :10 -ac -screen 0 1024x768x24 &
+echo | Xvfb :10 -ac -screen 0 1024x768x24 &  # WARM RESTART
 
 # Make sure DISPLAY=:10 is a permanent environment variable
 echo DISPLAY=:10 >> ~/.bashrc
@@ -19,7 +19,7 @@ echo DISPLAY=:10 >> /etc/environment
 source ~/.bashrc
 source ~/.profile
 
-x11vnc -ncache 10 -ncache_cr -display :10 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6
+x11vnc -ncache 10 -ncache_cr -display :10 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6  # WARM RESTART
 
 # download installation script
 wget https://download2.interactivebrokers.com/installers/ibgateway/stable-standalone/ibgateway-stable-standalone-linux-x64.sh
@@ -43,7 +43,7 @@ chmod o+x /opt/ibc/*.sh /opt/ibc/*/*.sh
 DISPLAY=:10 /opt/ibc/gatewaystart.sh
 
 # Restart IB Gateway if it shuts off
-crontab /opt/ibc/crontab.txt
+crontab /opt/ibc/crontab.txt  # WARM RESTART
 
 # # Install Python
 apt-get install -y python3 python3-dev python3-venv gcc  # gcc is for bt dependency
@@ -52,6 +52,6 @@ python3 get-pip.py
 
 pip install jupyterlab
 
-jupyter lab --allow-root --no-browser --config=$CONFIG_DIR/jupyter_notebook_config.py --port=8888
+jupyter lab --allow-root --no-browser --config=$CONFIG_DIR/jupyter_notebook_config.py --port=8888  # WARM RESTART
 
 echo 'Done! You can now access IB through JupyterLab'
